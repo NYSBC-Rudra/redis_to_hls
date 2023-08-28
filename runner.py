db9339860faa97c506bdefde60c2b6b3d87a98c9
NYSBC-Rudra <133771854+NYSBC-Rudra@users.noreply.github.com>
2023-08-28 15:13:45 -0400

    Update runner.py

--- runner.py
+++ runner.py
@@ -5,6 +5,6 @@ from imagestreamer import ImagetoVideoStreamer
 os.chdir('live_server')
 os.system('rm -r *.m3u8')
 os.system('rm -r *.ts')
-subprocess.Popen(['python3', '-m', 'http.server', '8000', '--bind', '127.0.0.1'])
+subprocess.Popen(['python3', '-m', 'http.server', '--bind', '3909',  '10.67.147.26'])
 os.chdir('..')
 newstreamer = ImagetoVideoStreamer()
