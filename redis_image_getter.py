78cb521dc7852189d50eb0cf31de3f59522bec6b
NYSBC-Rudra <133771854+NYSBC-Rudra@users.noreply.github.com>
2023-08-30 10:59:01 -0400

    Update redis_image_getter.py

--- redis_image_getter.py
+++ redis_image_getter.py
@@ -68,61 +68,59 @@ def getImage():
 # app.exec_()
 
 class App(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.title = 'My Screen'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
-        self.initUI()
+#     def __init__(self):
+#         super().__init__()
+#         self.title = 'My Screen'
+#         self.left = 10
+#         self.top = 10
+#         self.width = 640
+#         self.height = 480
+#         self.initUI()
 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.label = QtWidgets.QLabel(self)
-        self.last_x, self.last_y = None, None
-        canvas = QtGui.QPixmap(1224, 1020)
-        self.label.setPixmap(canvas)
-        timer = QtCore.QTimer(self)
-        timer.timeout.connect(self.update_image)
-        timer.start(60*1000)
-        self.update_image()
+#     def initUI(self):
+#         self.setWindowTitle(self.title)
+#         self.setGeometry(self.left, self.top, self.width, self.height)
+#         self.label = QtWidgets.QLabel(self)
+#         self.last_x, self.last_y = None, None
+#         canvas = QtGui.QPixmap(1224, 1020)
+#         self.label.setPixmap(canvas)
+#         timer = QtCore.QTimer(self)
+#         timer.timeout.connect(self.update_image)
+#         timer.start(60*1000)
+#         self.update_image()
 
-    def update_image(self):
-        pixmap = QtGui.QPixmap(getImage())
-        if not pixmap.isNull():
-            self.label.setPixmap(pixmap)
-            self.label.adjustSize()
-            self.resize(pixmap.size())
+#     def update_image(self):
+#         pixmap = QtGui.QPixmap(getImage())
+#         if not pixmap.isNull():
+#             self.label.setPixmap(pixmap)
+#             self.label.adjustSize()
+#             self.resize(pixmap.size())
 
     
-    def mouseMoveEvent(self, e):
-        if self.last_x is None: # First event.
-            self.last_x = e.x()
-            self.last_y = e.y()
-            return # Ignore the first time.
+#     def mouseMoveEvent(self, e):
+#         if self.last_x is None: # First event.
+#             self.last_x = e.x()
+#             self.last_y = e.y()
+#             return # Ignore the first time.
 
-        canvas = self.label.pixmap()
-        painter = QtGui.QPainter(canvas)
-        painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
-        painter.end()
-        self.label.setPixmap(canvas)
+#         canvas = self.label.pixmap()
+#         painter = QtGui.QPainter(canvas)
+#         painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
+#         painter.end()
+#         self.label.setPixmap(canvas)
 
-        # Update the origin for next time.
-        self.last_x = e.x()
-        self.last_y = e.y()
+#         # Update the origin for next time.
+#         self.last_x = e.x()
+#         self.last_y = e.y()
 
-    def mouseReleaseEvent(self, e):
-        self.last_x = None
-        self.last_y = None
-
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    ex = App()
-    ex.show()
-    sys.exit(app.exec_())
+#     def mouseReleaseEvent(self, e):
+#         self.last_x = None
+#         self.last_y = None
 
 
+# if __name__ == '__main__':
+#     app = QtWidgets.QApplication(sys.argv)
+#     ex = App()
+#     ex.show()
+#     sys.exit(app.exec_())
 
